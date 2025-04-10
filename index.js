c79f1df63a4d595b0f7f3bbc242fbe6f1d910b58
ftanekha <farai.tanekha@gmail.com>
2025-04-10 05:14:37 +0100

    - add second table with 5 max numbers per line

--- index.js
+++ index.js
@@ -65,4 +65,60 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     var getLucky = document.getElementById('get-lucky');
     getLucky.addEventListener('click', displayNumbers);
+    // /////////////////////////////////////////////////////
+    function subDisplayNumbers() {
+        function getRandomNumbers(max, maxLines) {
+            if (maxLines === void 0) { maxLines = 5; }
+            //create a line of unique numbers
+            function getLine(array) {
+                if (array === void 0) { array = []; }
+                var myNumbers = __spreadArray([], array, true);
+                if (myNumbers.length >= 5)
+                    return myNumbers;
+                var nextNumber = Math.ceil(Math.random() * max);
+                //enforce uniqueness
+                if (myNumbers.includes(nextNumber)) {
+                    return getLine(myNumbers);
+                }
+                else {
+                    myNumbers.push(nextNumber);
+                    return getLine(myNumbers);
+                }
+            }
+            var ticket = [];
+            //sort numbers in ascending order
+            for (var line = 0; line < maxLines; line++) {
+                ticket.push(getLine().sort(function (a, b) { return a - b; }));
+            }
+            return ticket;
+        }
+        var randomNumbers = getRandomNumbers(50);
+        randomNumbers.forEach(function (line, index) {
+            var targetEl = document.getElementById("".concat(index + 6));
+            //clear the ticket
+            targetEl.innerHTML = '';
+            //render the lines
+            line.forEach(function (number) {
+                var listItem = document.createElement('li');
+                listItem.className = "w-1/6 bg-gray-".concat(11 - index, "00 text-gray-").concat(index + 1, "00 font-black text-lg text-center border-b-4 border-gray-").concat(index + 2, "00");
+                listItem.textContent = String(number);
+                targetEl === null || targetEl === void 0 ? void 0 : targetEl.append(listItem);
+            });
+        });
+        document.getElementById('sub-numbers-container').style.height = '500px';
+        var timer = setTimeout(function () {
+            var button = document.getElementById('sub-get-lucky');
+            if (button.textContent === getLuckyText) {
+                button.textContent = goodLuckText;
+                button.classList.replace('text-gray-300', 'text-amber-500');
+            }
+            setTimeout(function () {
+                button.textContent = getLuckyText;
+                button.classList.replace('text-amber-500', 'text-gray-300');
+                clearTimeout(timer);
+            }, 10000);
+        }, 2000);
+    }
+    var subGetLucky = document.getElementById('sub-get-lucky');
+    subGetLucky.addEventListener('click', subDisplayNumbers);
 });
--- index.ts
+++ index.ts
@@ -75,5 +75,78 @@ document.addEventListener(
         getLucky.addEventListener(
             'click', displayNumbers
         )
+        ///////////////////////////////////////////////////////
+        function subDisplayNumbers(){
+            function getRandomNumbers(max: number, maxLines = 5):number[][] {
+                //create a line of unique numbers
+                function getLine(array: number[] = []):number[]{
+                    let myNumbers = [...array]
+    
+                    if(myNumbers.length >= 5) return myNumbers
+    
+                    const nextNumber = Math.ceil(Math.random() * max)
+                    //enforce uniqueness
+                    if(myNumbers.includes(nextNumber)) {
+                        return getLine(myNumbers)
+                    }else{
+                        myNumbers.push(nextNumber)
+                        return getLine(myNumbers)
+                    }
+                }
+                
+                const ticket: number[][] = []
+                //sort numbers in ascending order
+                for(let line = 0; line < maxLines; line++){
+                    ticket.push(getLine().sort((a,b)=> a - b))
+                }
+                return ticket
+            }
+            
+            const randomNumbers = getRandomNumbers(50)
+            
+            randomNumbers.forEach(
+                (line: number[], index: number) => {
+                    const targetEl = document.getElementById(`${index + 6}`)
+                    //clear the ticket
+                    targetEl.innerHTML = ''
+                    //render the lines
+                    line.forEach(
+                        number => {
+                            const listItem = document.createElement('li')
+                            listItem.className = `w-1/6 bg-gray-${11 - index}00 text-gray-${index + 1}00 font-black text-lg text-center border-b-4 border-gray-${index + 2}00`
+                            listItem.textContent = String(number)
+                            
+                            targetEl?.append(listItem)
+                        }
+                    )
+                }
+            )
+
+            document.getElementById('sub-numbers-container').style.height = '500px'
+
+            const timer = setTimeout(
+                ()=> {
+                    const button = document.getElementById('sub-get-lucky')
+                    if(button.textContent === getLuckyText) {
+                        button.textContent = goodLuckText
+                        button.classList.replace('text-gray-300', 'text-amber-500')
+                    }
+
+                    setTimeout(
+                        ()=> {
+                            button.textContent = getLuckyText
+                            button.classList.replace('text-amber-500', 'text-gray-300')
+                            clearTimeout(timer)
+                        },
+                        10000
+                    )
+                }, 
+                2000
+            )
+        }
+        const subGetLucky = document.getElementById('sub-get-lucky')
+        subGetLucky.addEventListener(
+            'click', subDisplayNumbers
+        )
     }
 )
